eb50ded9409f0003d3ccb231b3f41cc0a6b849a2
Kylee Want <Lumine3449@gmail.com>
2022-09-04 16:57:24 -0600

    everything but setInterval

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
-// Plan:
-/*
-*Each upgrade has different modifier values and prices
-*/
-
 const workers = [
   {
     name: 'Dave',
     level: 0,
-    price: 0,
+    price: 10,
     income: 0
   },
   {
     name: 'Zed',
     level: 0,
-    price: 0,
+    price: 20,
     income: 0
   },
   {
     name: 'Mary',
     level: 0,
-    price: 0,
+    price: 30,
     income: 0
   }
 ]
 
-let gold = 1249
+let gold = 10000
 let clickCount = 1
 let autoCount = 0
+
+let autoUpgradePrice1 = 200
+let autoUpgradePrice2 = 500
 
 let autoUpgrade1 = 200
 let autoUpgrade2 = 500
@@ -34,15 +32,6 @@ let autoUpgrade2 = 500
 let clickUpgrade1 = 50
 let clickUpgrade2 = 100
 
-
-
-// function buyWorker(workerBuy) {
-//   const worker = workers.find(w => w.name == workerBuy)
-//   if (gold >= worker.price) {
-//     gold -= worker.price
-//   }
-//   update()
-// }
 
 
 function levelDave() {
@@ -53,6 +42,7 @@ function levelDave() {
     worker.level++
     worker.price += 10
     worker.income++
+    autoCount++
   }
   update()
 }
@@ -65,6 +55,7 @@ function levelZed() {
     worker.level++
     worker.price += 20
     worker.income += 2
+    autoCount += 2
   }
   update()
 }
@@ -77,6 +68,7 @@ function levelMary() {
     worker.level++
     worker.price += 30
     worker.income += 3
+    autoCount += 3
   }
   update()
 }
@@ -93,6 +85,7 @@ function clickerUpgrade() {
   }
   update()
 }
+
 function clickerUpgrade2() {
   if (clickUpgrade2 <= gold) {
     gold -= clickUpgrade2
@@ -110,23 +103,20 @@ function automaticUpgrade1() {
     gold -= autoUpgrade1
     autoUpgrade1++
     autoCount++
+    autoUpgradePrice1++
   }
-
   update()
 }
-
 
 function automaticUpgrade2() {
   if (autoUpgrade2 <= gold) {
     gold -= autoUpgrade2
     autoUpgrade2++
-    autoCount == 1
     autoCount *= 2
+    autoUpgradePrice2++
   }
-
   update()
 }
-
 
 
 
@@ -144,6 +134,8 @@ function update() {
   const mary = workers.find(w => w.name == 'Mary')
 
   document.getElementById('totalGold').innerHTML = gold.toString()
+  document.getElementById('autoUpgradePrice1').innerHTML = autoUpgradePrice1.toString()
+  document.getElementById('autoUpgradePrice2').innerHTML = autoUpgradePrice2.toString()
 
   document.getElementById('DaveLevel').innerText = dave.level.toString()
   document.getElementById('daveLevelPrice').innerText = dave.price.toString()
@@ -162,7 +154,6 @@ function update() {
   document.getElementById('clickerUpgrade2').innerHTML = clickUpgrade2.toString()
   document.getElementById('clickerUpgrade1').innerHTML = clickUpgrade1.toString()
 }
-
 
 
 
